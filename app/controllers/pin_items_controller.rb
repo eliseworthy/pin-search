fa53b8b1d99c9ef7d44c3494a10193291f1ef261
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-27 15:16:13 -0400

    added ability to get all pins w/ no params

--- app/controllers/pin_items_controller.rb
+++ app/controllers/pin_items_controller.rb
@@ -3,7 +3,11 @@ class PinItemsController < ApplicationController
   # GET /pin_items.json
 
   def index
-    @pin_items = PinItem.pin_search(params[:search])
+  	if params[:search]
+      @pin_items = PinItem.pin_search(params[:search])
+    else
+      @pin_items = PinItem.all
+     end
     render json: @pin_items
   end
 end
